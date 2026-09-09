17d68ae21bcccc16bf2b9e9e7c1144fcf9a78518
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-09-09 00:55:36 +0000

    🚀 feat: iniciar estructura basica del proyecto

---
+++ src/ejercicios/ejercicio1.js
@@ -0,0 +1,3 @@
+export const imprimir = () =>{
+    console.log("hola")
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+export * from './ejercicios/ejercicio1.js';
+export * from './ejercicios/ejercicio2.js';
+export * from './ejercicios/ejercicio3.js';
+export * from './ejercicios/ejercicio4.js';
+export * from './ejercicios/ejercicio5.js';
+export * from './menu/menu.js';
