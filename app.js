b6b3d22e274c7b61e53180122eef0b82f0a8d13e
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-09-13 08:05:24 +0000

    feat: Se implmenta correctamente la lógica del menú

--- app.js
+++ app.js
@@ -1,47 +1,64 @@
-import {  } from './src/index.js'
-
 import promptSync from 'prompt-sync';
 
 const prompt = promptSync();
 
-let opcion =  parseInt(prompt("Ingrese el número del ejercicio que desea probar"))
+const escogerOfertas = (opcion) => {
 
-const opcionesDeMenu = ()=>{
-    do{
-        
-    console.log("Bienvenido al programa evaluación asincronía")
-    console.log("Ingrese un número según la opción que desea escoger")
-    console.log("1. Ejercicio 1");
-    console.log("2. Ejercicio 2");
-    console.log("3. Ejercicio 3");
-    console.log("4. Ejercicio 4");
-    console.log("6. Salir");
+    switch (opcion) {
 
-    }while(opcion != 6);
+        case 1:
+            console.log("Ejercicio 1");
+            break;
 
-}
+        case 2:
+            console.log("Ejercicio 2");
+            break;
 
-const escogerOfertas =(opcion )=>{
-    switch(opcion){
-        case 1 :
-            console.log("Ejercicio1")
+        case 3:
+            console.log("Ejercicio 3");
             break;
-        case 2 :
-            console.log("Ejercicio2")
+
+        case 4:
+            console.log("Ejercicio 4");
             break;
-        case 3 :
-            console.log("Ejercicio3")
+
+        case 5:
+            console.log("Ejercicio 5");
             break;
-        case 4 :
-            console.log("Ejercicio4")
+
+        case 6:
+            console.log("Saliendo...");
             break;
-            case 5 :
-            console.log("Ejercicio5")
-            break;
-              case 6 :
-            console.log("Saliendo")
-            break;
-            default:
-            console.log("Saliendo")
+
+        default:
+            console.log("Opción no válida");
     }
-}
+};
+
+
+const opcionesDeMenu = () => {
+
+    let opcion;
+
+    do {
+
+        console.log("\nBienvenido al programa evaluación asincronía");
+        console.log("Ingrese un número según la opción que desea escoger");
+        console.log("1. Ejercicio 1");
+        console.log("2. Ejercicio 2");
+        console.log("3. Ejercicio 3");
+        console.log("4. Ejercicio 4");
+        console.log("5. Ejercicio 5");
+        console.log("6. Salir");
+
+        opcion = parseInt(
+            prompt("Ingrese el número del ejercicio que desea probar: ")
+        );
+
+        escogerOfertas(opcion);
+
+    } while (opcion !== 6);
+};
+
+
+opcionesDeMenu();
