2c2f56c08ffab356291557bc56e87663cd592d27
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-09-15 05:32:52 +0000

    feat: listar tareas pendientes por usuarios registrados

--- src/ejercicios/ejercicio1.js
+++ src/ejercicios/ejercicio1.js
@@ -1,15 +1,35 @@
 /*
 1. Listar todas las tareas pendientes por cada usuario registrado en la API
 */
-import {api_url} from "../utils/config.js"
+import { api_url } from "../utils/config.js"
 
-export const listarTareasPendientesPorUsuariosRegistradosEnLaApi = async()=>{
-    try{
-const response = await fetch(`${api_url}/users`)
-const data = await response.json()
-console.log(data)     
+export const listarTareasPendientesPorUsuariosRegistradosEnLaApi = async () => {
+    try {
+        //Fetch para obtener usuarios
+        const respuestaUsers = await fetch(`${api_url}/users`)
+        const dataUsers = await respuestaUsers.json()
 
-    }catch(error){
+
+        //Fetch para obtener  Tareas
+        const respuestaTodos = await fetch(`${api_url}/todos`)
+        const dataTodos = await respuestaTodos.json()
+
+        //Recorrer usuarios 
+        for (const dataUser of dataUsers) {
+            console.log("*".repeat(30));
+            console.log("*".repeat(30));
+            console.log(`Usuario: ${dataUser.name}`)
+            console.log("Tareas pendientes: ")
+
+            for (const dataTodo of dataTodos) {
+                //Verificar tarea pertenezca al usuario y esté pendiente 
+                if (dataTodo.userId === dataUser.id && dataTodo.completed === false) {
+                    console.log(`-${dataTodo.title}`)
+
+                }
+            }
+        }
+    } catch (error) {
         console.log("Ocurrió un error", error)
     }
 }
