d806d8476d6155f0d7ca37a3e21cf7eb7f86b40b
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-09-16 01:16:56 +0000

    refactor: modularizar la logica del menu
    - Mover la logica y funciones del menu desde App.js hacia menu.js
    - Reducir la responsabilidad de App.js para mejorar la mantenibilidad del codigo

---
+++ src/menu/menu.js
@@ -0,0 +1,82 @@
+import promptSync from 'prompt-sync';
+
+import { listarTareasPendientesPorUsuariosRegistradosEnLaApi,
+		 listarUsuarioAlbumesFotos,
+		 listarPostConComentarios,
+		 listarUsuariosNombreTelefono,
+		 mostrarUsuariosCompletos } from "../index.js";
+
+
+const prompt = promptSync();
+
+
+export const opcionesDeMenu = async () => {
+
+	let opcion;
+
+	do {
+
+		console.log("Bienvenido al programa evaluación asincronía");
+		console.log("Ingrese un número según la opción que desea escoger");
+		console.log("1. Ejercicio 1");
+		console.log("2. Ejercicio 2");
+		console.log("3. Ejercicio 3");
+		console.log("4. Ejercicio 4");
+		console.log("5. Ejercicio 5");
+		console.log("6. Salir");
+
+		opcion = Number(prompt("Ingrese una opción: "));
+
+		switch (opcion) {
+
+			case 1:
+
+				console.log("Ejercicio 1");
+
+				await listarTareasPendientesPorUsuariosRegistradosEnLaApi();
+
+				break;
+
+
+			case 2:
+
+				console.log("Ejercicio 2");
+
+				await listarUsuarioAlbumesFotos();
+
+				break;
+
+
+			case 3:
+
+				console.log("Ejercicio 3");
+				await listarPostConComentarios();
+
+				break;
+
+
+			case 4:
+
+				console.log("Ejercicio 4");
+				await listarUsuariosNombreTelefono();
+				break;
+
+
+			case 5:
+
+				console.log("Ejercicio 5");
+
+				await mostrarUsuariosCompletos();
+
+				break;
+
+
+			case 6:
+
+				console.log("Programa finalizado.");
+
+				break;
+		}
+
+	} while (opcion !== 6);
+};
